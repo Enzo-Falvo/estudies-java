d0f95d866603e87c3bdaa961c9ca72ae8266fa0e
Enzo Falvo <bestwordno@gmail.com>
2020-05-21 00:36:23 -0300

    FileReader and BufferedReader

---
+++ FileReader-and-BufferedReader/src/filereader/and/bufferedreader/FileReaderAndBufferedReader.java
@@ -0,0 +1,53 @@
+
+package filereader.and.bufferedreader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderAndBufferedReader {
+    
+        
+    public static void main(String[] args) {
+        
+        String path = "c:\\intel\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+        
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+            
+            String line = br.readLine();
+            
+            while (line != null ) {
+                System.out.println(line);
+                br.readLine();
+            }
+            
+            
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            try {
+
+                if (br == null) {
+                    br.close();
+                }
+                if (fr == null) {
+                    fr.close();
+                }
+            }
+            catch (IOException e ) {
+                e.printStackTrace();
+            }
+        }
+
+        
+        
+        
+    }
+    
+}
