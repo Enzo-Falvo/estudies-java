260a463c93deac0e9401fab12805963bb05944aa
Enzo Falvo <bestwordno@gmail.com>
2020-06-19 22:36:50 -0300

    Declared lamda expression - Solution 4

--- Functional Programming And Lambda Expressions/Function-Interface/src/function/pkginterface/FunctionInterface.java
+++ Functional Programming And Lambda Expressions/Function-Interface/src/function/pkginterface/FunctionInterface.java
@@ -4,6 +4,7 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import util.UpperCaseName;
 
@@ -18,7 +19,10 @@ public class FunctionInterface {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
         
-       List <String> names = list.stream().map(Product :: nonStaticUpperCaseName).collect(Collectors.toList());
+        
+       Function <Product, String> funk = p -> p.getName().toUpperCase();
+        
+       List <String> names = list.stream().map(funk).collect(Collectors.toList());
        
        names.forEach(System.out :: println);
 
