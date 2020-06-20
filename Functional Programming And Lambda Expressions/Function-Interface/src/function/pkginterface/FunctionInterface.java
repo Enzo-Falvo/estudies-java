cf19d6659815a6c65728dc79f9b3b30320547f84
Enzo Falvo <bestwordno@gmail.com>
2020-06-19 22:47:01 -0300

    Inline lambda expression - Solution 5

--- Functional Programming And Lambda Expressions/Function-Interface/src/function/pkginterface/FunctionInterface.java
+++ Functional Programming And Lambda Expressions/Function-Interface/src/function/pkginterface/FunctionInterface.java
@@ -20,9 +20,9 @@ public class FunctionInterface {
         list.add(new Product("HD Case", 80.90));
         
         
-       Function <Product, String> funk = p -> p.getName().toUpperCase();
+       
         
-       List <String> names = list.stream().map(funk).collect(Collectors.toList());
+       List <String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
        
        names.forEach(System.out :: println);
 
