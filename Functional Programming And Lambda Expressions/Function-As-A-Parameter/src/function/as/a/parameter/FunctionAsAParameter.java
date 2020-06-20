f445e09bce07f0216dc3e264144a64a06e704113
Enzo Falvo <bestwordno@gmail.com>
2020-06-20 16:10:13 -0300

    Function as a parameter

---
+++ Functional Programming And Lambda Expressions/Function-As-A-Parameter/src/function/as/a/parameter/FunctionAsAParameter.java
@@ -0,0 +1,26 @@
+package function.as.a.parameter;
+
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+import util.ProductService;
+
+public class FunctionAsAParameter {
+
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+        
+        
+        ProductService ps = new ProductService();
+        
+        double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'M');
+        
+        System.out.println("Sum: " + String.format("%.2f", sum));
+        
+    }
+
+}
